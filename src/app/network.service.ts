9bdf138f5301eb944b28e408ac9ce26630a8ac7f
Marvelous Ighodaro <70376880+marvelwilson@users.noreply.github.com>
2022-10-03 23:22:24 +0100

    Update network.service.ts

--- src/app/network.service.ts
+++ src/app/network.service.ts
@@ -6,8 +6,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 })
 export class NetworkService {
 
-  url = 'http://localhost:8000/api/';
-  // url = 'https://agilfinance.net/service/api/';
+  // url = 'http://localhost:8000/api/';
+  url = 'https://agilfinance.net/service/api/';
 
   token: any;
   bearer: any;
